d3fca6324723165bbb666a823415a21031535554
Justin Cohen <justin.cohen@handshake.com>
2016-01-14 21:29:13 -0500

    adds cron job, adds logging

---
+++ index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+let CronJob = require('cron').CronJob,
+    pingSites = require('./ping'),
+    fs = require('fs');
+
+/*
+    Pings at 9AM and 6PM each day
+    texts me if errors
+    writes results to log
+*/
+
+let job = new CronJob({
+    cronTime: "* * 9,18 * * *",
+    onTick: pingSites,
+    start: true,
+    timeZone: 'America/New_York'
+});
--- ping.js
+++ ping.js
@@ -2,6 +2,7 @@
 
 let request = require('request'),
     async = require('async'),
+    fs = require('fs'),
     settings = require('./settings');
 
 let sites = settings.sites,
@@ -21,20 +22,40 @@ function sendText(body){
     )
 }
 
-//ping my projects, send text if error
-async.each(sites, (site, callback) => {
-    request.get(site, (err, res, body) => {
-        if(err){
-            statuses[site] = res.statusCode;
-            callback(err);
-        }
-
-        statuses[site] = res.statusCode;
-        callback();
+//Create/append to log
+function logResults(results){
+    fs.appendFile('log.txt', results +'\n', (err)=>{
+        if(err){ return; }//oh well
     });
-}, function(err){
-        if(err){
-            sendText(JSON.stringify(statuses));
+}
+
+//ping my projects, send text if error
+function pingSites(){
+    async.each(sites, (site, callback) => {
+        request.get(site, (err, res, body) => {
+            if(err){
+                statuses[site] = res.statusCode;
+                callback(err);
+            }
+
+            statuses[site] = res.statusCode;
+            callback();
+        });
+    }, function(err){
+            if(err){
+                sendText(JSON.stringify(statuses));
+                return;
+            }
+
+            // write to log on success
+            let res = {};
+            res[Date()] = statuses;
+            logResults(JSON.stringify(res))
+
         }
-    }
-)
+    )
+}
+
+pingSites()
+
+module.exports = pingSites;
